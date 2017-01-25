7d43858b0763474b3da18ed5810642b189988bb1
Armored Dragon <Armored-Dragon@users.noreply.github.com>
2017-01-25 15:12:45 -0800

    Add ZWSP for good measure

--- util/goldbot.py
+++ util/goldbot.py
@@ -437,7 +437,7 @@ class GoldBot(commands.Bot):
             twitch_global = json.loads(f.read())['emotes']
         with open(os.path.join(cur_dir, 'assets', 'emotes_twitch_subscriber.json')) as f:
             twitch_sub = json.loads(f.read())
-        twitch_subcriber = {e: {'description': '', 'image_id': twitch_sub[e], 'first_seen': None} for e in twitch_sub}
+        twitch_subcriber = {e: {'description': '\u200b', 'image_id': twitch_sub[e], 'first_seen': None} for e in twitch_sub}
         self.emotes['twitch'] = {**twitch_global, **twitch_subscriber}
         with open(os.path.join(cur_dir, 'assets', 'emotes_ffz.json')) as f:
             self.emotes['ffz'] = json.loads(f.read())
