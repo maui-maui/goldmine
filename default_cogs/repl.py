e2a9e89c9dff0b1fd63e9f409fd221389dad6371
Armored Dragon <Armored-Dragon@users.noreply.github.com>
2017-01-28 09:01:30 -0800

    fix public repl

--- default_cogs/repl.py
+++ default_cogs/repl.py
@@ -131,7 +131,7 @@ class REPL(Cog):
             del variables['bot']
             del variables['ctx'].bot
             checks = {}
-            ex_check = lambda m: (m.author.id != self.bot.user.id) and not m.bot
+            ex_check = lambda m: (m.author.id != self.bot.user.id) and not m.author.bot
         else:
             checks = {
                 'author': msg.author
