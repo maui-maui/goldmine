d1ab01ec787cf4a201d15fd47e95eaa6bbd5eb47
Armored Dragon <Armored-Dragon@users.noreply.github.com>
2017-02-15 15:56:15 -0800

    meh

--- default_cogs/moderation.py
+++ default_cogs/moderation.py
@@ -23,10 +23,10 @@ class Moderation(Cog):
                     return
                 await self.bot.send_message(msg.channel, msg.author.mention + \
                                             ' **:japanese_goblin: Stop crashing iOS users!**')
-        if msg.content.lower() == 'kys':
-            if msg.server.id != '110373943822540800': # DBots
-                await self.bot.msend(msg, 'Ay ' + msg.author.mention + \
-                                     ', follow your own advice first, :ok_hand::question:')
+#        if msg.content.lower() == 'kys':
+#            if msg.server.id != '110373943822540800': # DBots
+#                await self.bot.msend(msg, 'Ay ' + msg.author.mention + \
+#                                     ', follow your own advice first, :ok_hand::question:')
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
